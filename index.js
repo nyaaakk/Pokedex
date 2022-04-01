02eea7fe2f715be1dca4815ae74abb9f0ffb2855
nyaaakk <ryannpicard@gmail.com>
2022-04-01 02:29:50 +0200

    pokedex finit

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ async function getPokeList(){
             let imgPoke = document.createElement("img");
 
             imgPoke.src = `assets/poke-icons/${i+1}.png`;
-            //option.setAttribute("onclick", `getPokeInfo(${i+1})`);
+            option.setAttribute("onclick", `getPokeInfo(${i+1})`);
             option.innerHTML = `${i+1} - ${result.results[i].name}`;
             option.appendChild(imgPoke);
             pokeList.appendChild(option);
@@ -33,19 +33,69 @@ async function getPokeList(){
 
 };
 
-/*async function getPokeInfo(noPokemon){
+async function getPokeInfo(noPokemon){
     let pokemon = getPokemon(noPokemon);
+    let imgPoke = document.getElementById("main-image");
+    let iconPoke = document.getElementById("poke-icon");
+    let nomPoke = document.getElementById("poke-title");
+    let taillePoke = document.getElementById("poke-height");
+    let poidsPoke = document.getElementById("poke-weight");
+    let typePoke = document.getElementById("poke-types");
+    let hpPoke = document.getElementById("hp");
+    let attPoke = document.getElementById("att");
+    let defPoke = document.getElementById("def");
+    let spePoke = document.getElementById("spe");
+    let resPoke = document.getElementById("res");
+    let spdPoke = document.getElementById("spd");
+    typePoke.innerHTML = "";
 
-};*/
+    pokemon.then((result) => {
 
-function init(){
-    getPokeList();
+        imgPoke.src = `assets/poke-img/${noPokemon}.png`;
+        iconPoke.src = `assets/poke-icons/${noPokemon}.png`;
+        nomPoke.innerHTML = `#${noPokemon} - ${result.name}`;
+        taillePoke.innerHTML = `${result.height/10} m`;
+        poidsPoke.innerHTML = `${result.weight/10} kg`;
+
+        result.types.forEach(element => {
+            let imgType = document.createElement("img");
+            imgType.src = `assets/poke-types/${element.type.name}.gif`;
+            typePoke.appendChild(imgType);
+        });
+
+        result.stats.forEach(element => {
+            hpPoke.setAttribute("style", `width: ${result.stats[0].base_stat}px`);
+            hpPoke.innerHTML = `${result.stats[0].base_stat}`;
+
+            attPoke.setAttribute("style", `width: ${result.stats[1].base_stat}px`);
+            attPoke.innerHTML = `${result.stats[1].base_stat}`;
+
+            defPoke.setAttribute("style", `width: ${result.stats[2].base_stat}px`);
+            defPoke.innerHTML = `${result.stats[2].base_stat}`;
+
+            spePoke.setAttribute("style", `width: ${result.stats[3].base_stat}px`);
+            spePoke.innerHTML = `${result.stats[3].base_stat}`;
+
+            resPoke.setAttribute("style", `width: ${result.stats[4].base_stat}px`);
+            resPoke.innerHTML = `${result.stats[4].base_stat}`;
+
+            spdPoke.setAttribute("style", `width: ${result.stats[5].base_stat}px`);
+            spdPoke.innerHTML = `${result.stats[5].base_stat}`;
+        });
+
+    });
+
 };
 
-function showLogin(){
-    document.getElementById('login-modal').style.display = 'block';
-}
+async function init(){
+    getPokeList();
+    getPokeInfo(1);
+};
 
-function closeLogin(){
+async function showLogin(){
+    document.getElementById('login-modal').style.display = 'block';
+};
+
+async function closeLogin(){
   document.getElementById('login-modal').style.display = 'none';
-}
+};
